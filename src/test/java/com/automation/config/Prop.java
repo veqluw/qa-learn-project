80b106b43edecd0874b451a85a61c7859199170d
veqluw <mzaboronok@icloud.com>
2026-05-18 12:13:22 +0300

    add props

---
+++ src/test/java/com/automation/config/Prop.java
@@ -0,0 +1,63 @@
+package com.automation.config;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.LoadPolicy;
+import org.aeonbits.owner.Config.LoadType;
+import org.aeonbits.owner.Config.Sources;
+import org.aeonbits.owner.ConfigFactory;
+
+public class  Prop {
+
+    private static Class<? extends PropInterface> getPropertySource() {
+        String env = System.getProperty("env");
+        if (env == null || env.equals("null")) {
+            return PropInterfaceTest.class;
+        } else if (env.equals("test")) {
+            return PropInterfaceTest.class;
+        } else {
+            throw new RuntimeException("Invalid value for system property 'env'! Expected one of:[null, 'test']");
+        }
+    }
+
+    public static final PropInterface PROP = ConfigFactory.create(getPropertySource());
+
+    @LoadPolicy(LoadType.MERGE)
+    @Sources({"system:properties", "classpath:test.yaml"})
+    interface PropInterfaceTest extends PropInterface {
+    }
+
+    public interface PropInterface extends Config {
+
+        // Web tests properties
+        @Key("webBrowserName")
+        String getBrowserName();
+
+        @Key("reqresBaseUrl")
+        String getReqresBaseUrl();
+
+        @Key("reqresToken")
+        String getReqresToken();
+
+        @Key("webBrowserVersion")
+        String getBrowserVersion();
+
+        @Key("demoqaBaseUrl")
+        String getDemoqaBaseUrl();
+
+        @Key("webBrowserSize")
+        String getBrowserSize();
+
+        @Key("webIsRemote")
+        Boolean isRemote();
+
+        @Key("webRemoteUrl")
+        String getRemoteUrl();
+
+        @Key("webPageLoadTimeout")
+        Long getPageLoadTimeout();
+
+        @Key("webTimeout")
+        Long getTimeout();
+    }
+
+}
